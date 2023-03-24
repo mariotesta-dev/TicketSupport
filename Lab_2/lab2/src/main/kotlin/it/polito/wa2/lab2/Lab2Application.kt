4312152cf5f3a3dacd15cb17a5da62c09f63ade9
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-03-24 13:34:28 +0100

    empty project

---
+++ Lab_2/lab2/src/main/kotlin/it/polito/wa2/lab2/Lab2Application.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.lab2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab2Application
+
+fun main(args: Array<String>) {
+	runApplication<Lab2Application>(*args)
+}
