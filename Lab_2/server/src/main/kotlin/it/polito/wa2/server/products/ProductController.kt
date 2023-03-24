d70d0a7913471220f11ac1a127d772d6c1c18ae2
Mario Testa <mario.testa1999@gmail.com>
2023-03-24 16:23:23 +0100

    api/products & api/products/{ean} added

---
+++ Lab_2/server/src/main/kotlin/it/polito/wa2/server/products/ProductController.kt
@@ -0,0 +1,18 @@
+package it.polito.wa2.server.products
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ProductController(private val productService: ProductService) {
+    @GetMapping("/API/products")
+    fun getAll() : List<ProductDTO> {
+        return productService.getAll()
+    }
+
+    @GetMapping("/API/products/{ean}")
+    fun getAll(@PathVariable ean: String) : ProductDTO? {
+        return productService.getProduct(ean)
+    }
+}
