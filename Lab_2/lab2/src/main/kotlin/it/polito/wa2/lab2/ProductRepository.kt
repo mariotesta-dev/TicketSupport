f58a34258b7fafdd9cdcc9f00b5f58974aba8cf5
Mattia Mazzari <71759564+mattiamazzari@users.noreply.github.com>
2023-03-24 13:53:11 +0100

    Delete Lab_2/lab2 directory

--- Lab_2/lab2/src/main/kotlin/it/polito/wa2/lab2/ProductRepository.kt
+++
@@ -1,8 +0,0 @@
-package it.polito.wa2.productApp.products
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ProductRepository: JpaRepository<Product,String> {
-}
--- Lab_2/lab2/src/main/kotlin/it/polito/wa2/lab2/ProductService.kt
+++
@@ -1,6 +0,0 @@
-package it.polito.wa2.productApp.products
-
-interface ProductService {
-    fun getAll(): List<ProductDTO>
-    fun getProduct(ean: String): ProductDTO?
-}
