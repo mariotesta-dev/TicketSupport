7a1e4f5daa8957ab1a9f4626cab1b483dce85e75
Mario Testa <mario.testa1999@gmail.com>
2023-07-20 12:49:27 +0200

    start final project

---
+++ Lab_4/client/src/views/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashbar from "../components/Dashbar";
+import { Stack } from "@chakra-ui/react";
+
+function Dashboard() {
+	return (
+		<Stack>
+			<Dashbar />
+		</Stack>
+	);
+}
+
+export default Dashboard;
